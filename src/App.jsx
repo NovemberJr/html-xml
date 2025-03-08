55b20ed79add6ebb45bea2b60d48fbbe1b933918
NovemberJr <jr.november@gmail.com>
2025-03-08 19:54:25 +0700

    Added sheet library

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import * as XLSX from "xlsx";
+
+import { HotTable, HotColumn } from "@handsontable/react-wrapper";
+import { registerAllModules } from "handsontable/registry";
+import "handsontable/styles/handsontable.min.css";
+import "handsontable/styles/ht-theme-main.min.css";
+
+registerAllModules();
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [data, setData] = useState({});
+	const [width, setWidth] = useState(0);
+	const [mergeCells, setMergeCells] = useState([]);
+	const [loading, setLoading] = useState(true);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => {
+		fetch("/upd-2024.xlsx")
+			.then((res) => {
+				return res.arrayBuffer();
+			})
+			.then((data) => {
+				const workbook = XLSX.read(data, { type: "array" });
+				const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+
+				const json = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+				setData(json);
+				console.log(json);
+
+				const width = json.reduce(
+					(max, arr) => Math.max(max, arr.length),
+					0
+				);
+				setWidth(width);
+
+				const merges = worksheet["!merges"] || [];
+				const mergesConverted = merges
+					.map(({ s, e }) => ({
+						row: s.r,
+						col: s.c,
+						rowspan: e.r - s.r + 1,
+						colspan: e.c - s.c + 1,
+					}))
+					.slice(61, 62);
+				setMergeCells(mergesConverted);
+				console.log(mergesConverted);
+			})
+			.then(() => {
+				setLoading(false);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
+
+	const arr = new Array(width).fill(null);
+
+	return (
+		<div className="ht-theme-main-dark-auto">
+			{!loading && (
+				<HotTable
+					data={data}
+					headerClassName="htLeft"
+					colHeaders={true}
+					rowHeaders={true}
+					licenseKey="non-commercial-and-evaluation"
+					mergeCells={mergeCells}
+				>
+					{arr.map((_, i) => (
+						<HotColumn key={i} />
+					))}
+				</HotTable>
+			)}
+		</div>
+	);
 }
 
-export default App
+export default App;
